627f91ebd37d086bdd208d6a0b2c6b6a24d05f14
Gery Pan <gerypandelieva@gmail.com>
2026-07-03 10:04:55 +0300

    Implement frontend Reservation form

---
+++ hotel_system/forms.py
@@ -0,0 +1,12 @@
+﻿from django import forms
+from .models import Reservation
+
+class ReservationForm(forms.ModelForm):
+    class Meta:
+        model = Reservation
+        fields = ['room', 'guest', 'number_of_guests', 'check_in', 'check_out']
+        # adding calendar attributes
+        widgets = {
+            'check_in': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'check_out': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+        }
--- hotel_system/views.py
+++ hotel_system/views.py
@@ -1,6 +1,21 @@
-﻿from django.shortcuts import render
+﻿from django.shortcuts import render, redirect
 from .models import Room
+from .forms import ReservationForm
 
 def home(request):
-    rooms = Room.objects.all() # all rooms from the database
-    return render(request, 'hotel_system/home.html', {'rooms': rooms}) # home.html template with rooms context
+    rooms = Room.objects.all()
+    
+    # Handle form submission
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save() # save the reservation to the database
+            return redirect('home') # reload the page to show the updated list of rooms
+    else:
+        # Show an empty form for requests
+        form = ReservationForm()
+
+    return render(request, 'hotel_system/home.html', {
+        'rooms': rooms,
+        'form': form
+    })
