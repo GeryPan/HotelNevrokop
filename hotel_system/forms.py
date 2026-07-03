4c954ce639acc64c9f9482cceff278dde647b195
Gery Pan <gerypandelieva@gmail.com>
2026-07-03 10:46:36 +0300

    More Bootstrap classes for uniform styling

--- hotel_system/forms.py
+++ hotel_system/forms.py
@@ -7,6 +7,9 @@ class ReservationForm(forms.ModelForm):
         fields = ['room', 'guest', 'number_of_guests', 'check_in', 'check_out']
         # adding calendar attributes
         widgets = {
+            'room': forms.Select(attrs={'class': 'form-select'}),
+            'guest': forms.Select(attrs={'class': 'form-select'}),
+            'number_of_guests': forms.NumberInput(attrs={'class': 'form-control', 'min': '1'}),
             'check_in': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
             'check_out': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
         }
