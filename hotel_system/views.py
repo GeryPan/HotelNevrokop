b49c2eca21f70e5ecee33cc1bdd8bd865e17f9c7
Gery Pan <gerypandelieva@gmail.com>
2026-07-03 10:25:33 +0300

    Add messages to display total price on successful reservation and show validation errors

--- hotel_system/views.py
+++ hotel_system/views.py
@@ -1,18 +1,25 @@
 ﻿from django.shortcuts import render, redirect
+from django.contrib import messages 
 from .models import Room
 from .forms import ReservationForm
 
 def home(request):
     rooms = Room.objects.all()
-    
-    # Handle form submission
     if request.method == 'POST':
         form = ReservationForm(request.POST)
         if form.is_valid():
-            form.save() # save the reservation to the database
-            return redirect('home') # reload the page to show the updated list of rooms
+            # save the reservation and calculate the total price
+            reservation = form.save() 
+            messages.success(
+                request, 
+                f"Успешна резервация! Вашата крайна сметка за престоя е: {reservation.total_price} лв."
+            )
+            return redirect('home')
+        else:
+            # shaw error messages
+            for error in form.errors.values():
+                messages.error(request, error)
     else:
-        # Show an empty form for requests
         form = ReservationForm()
 
     return render(request, 'hotel_system/home.html', {
